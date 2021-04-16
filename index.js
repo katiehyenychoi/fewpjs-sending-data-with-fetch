32c496929529ddddf013ae4d62805e4df2027660
Katie Hyewon Choi <hyewonchoi31@gmail.com>
2021-04-15 21:59:25 -0400

    commit tree 01c0ff549447167959c71a0da1de50810d250640

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Add your code here
+
+function submitData(userName, userEmail) {
+    let userInfo = {
+        name: userName,
+        email: userEmail
+    };
+
+    let configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(userInfo)
+    };
+
+    return fetch("http://localhost:3000/users", configurationObject)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (object) {
+            console.log(object);
+
+            let idToBeAppended = object.id
+            console.log(idToBeAppended)
+
+            let idLocation = document.querySelector("body");
+            console.log(idLocation);
+            idLocation.append(idToBeAppended);
+            document.body.innerHTML = idToBeAppended;
+        })
+        .catch(function (error) {
+            alert("Errorrrrr");
+            console.log(error.message);
+            let errorLocation = document.querySelector("body");
+            errorLocation.append(error.message);
+
+        });
+
+
+
+}
